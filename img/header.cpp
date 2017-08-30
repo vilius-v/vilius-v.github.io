8bb5d27d0af670bfb9a3723856f2178432ce4013
Vilius Vysniauskas <vysniauskasv@ucla.edu>
2017-08-30 00:16:35 -0700

    Initial commit: Header and menu navigation complete, with no JS required to dynamically change contents based on selected menu

---
+++ img/header.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void lifeGoals(Person p) {
+    if (me.state() == "sad") {
+        me.setState("happy");
+    }
+    cout << me.state();
+}
+
+int main() {
+    Person me = new Person("Vilius Vysniauskas");
+    lifeGoals(me);
+    return 0;
+}
+
+
+
+
+
